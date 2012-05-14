aed6262aadfd55a876f2386118ed016616bed99f
dgorlov <dgorlov@skbkontur.ru>
2012-05-14 11:29:58 +0000

    test

--- GroBuf/Tests/TestMultipleReadWrite.cs
+++ GroBuf/Tests/TestMultipleReadWrite.cs
@@ -16,11 +16,12 @@ namespace GroBuf.Tests
         [Test]
         public void Test()
         {
-            var data = serializer.Serialize("zzz", 100, new[] {1, 2, 3});
+            byte[] data = serializer.Serialize("zzz", 100, new[] {1, 2, 3});
             int index = 0;
             Assert.AreEqual("zzz", serializer.Deserialize<string>(data, ref index));
             Assert.AreEqual(100, serializer.Deserialize<int>(data, ref index));
             serializer.Deserialize<int[]>(data, ref index).AssertEqualsTo(new[] {1, 2, 3});
+            Assert.AreEqual(index, data.Length);
         }
 
         private ISerializer serializer;
