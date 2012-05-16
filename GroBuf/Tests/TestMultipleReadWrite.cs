6256c51ba142ac97d1d6a4e901a2f9ed7a49a0c9
dgorlov <dgorlov@skbkontur.ru>
2012-05-16 04:48:41 +0000

    test

--- GroBuf/Tests/TestMultipleReadWrite.cs
+++ GroBuf/Tests/TestMultipleReadWrite.cs
@@ -24,6 +24,43 @@ namespace GroBuf.Tests
             Assert.AreEqual(index, data.Length);
         }
 
+        [Test]
+        public void TestBug()
+        {
+            byte[] data = serializer.Serialize(new A {Q = 1, Z = 2}, new B {X = 3, Y = 4});
+            int index = 0;
+            var aChanged = serializer.Deserialize<AChanged>(data, ref index);
+            var bChanged = serializer.Deserialize<BChanged>(data, ref index);
+            Assert.AreEqual(1, aChanged.Q);
+            Assert.AreEqual(0, bChanged.Z);
+            Assert.AreEqual(3, bChanged.X);
+            Assert.AreEqual(4, bChanged.Y);
+        }
+
+        public class A
+        {
+            public int Q { get; set; }
+            public int Z { get; set; }
+        }
+
+        public class B
+        {
+            public int X { get; set; }
+            public int Y { get; set; }
+        }
+
+        public class AChanged
+        {
+            public int Q { get; set; }
+        }
+
+        public class BChanged
+        {
+            public int Z { get; set; }
+            public int X { get; set; }
+            public int Y { get; set; }
+        }
+
         private ISerializer serializer;
     }
 }
