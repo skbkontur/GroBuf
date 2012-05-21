80371cd2370ad99472cb9f534eb29ed3d78e6458
dgorlov <dgorlov@skbkontur.ru>
2012-05-21 05:15:36 +0000

    не работают non-public properties

---
+++ GroBuf/Tests/TestNonPublicProperties.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+using GroBuf.DataMembersExtracters;
+
+using NUnit.Framework;
+
+namespace GroBuf.Tests
+{
+    [TestFixture]
+    public class TestNonPublicProperties
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            serializer = new SerializerImpl(new AllPropertiesExtracter());
+        }
+
+        [Test]
+        public void TestStupid()
+        {
+            byte[] serialize = serializer.Serialize(new CWithnonPublics(2378, 3434));
+            var result = serializer.Deserialize<CWithnonPublics>(serialize);
+            Assert.AreEqual(2378, result.A);
+            Assert.AreEqual(3434, result.GetB());
+        }
+
+        public class AllPropertiesExtracter : IDataMembersExtracter
+        {
+            public MemberInfo[] GetMembers(Type type)
+            {
+                return type.GetProperties(BindingFlags.Public | BindingFlags.Instance).Where(property => property.CanRead && property.CanWrite).ToArray();
+            }
+        }
+
+        public class CWithnonPublics
+        {
+            public CWithnonPublics()
+            {
+            }
+
+            public CWithnonPublics(int a, int b)
+            {
+                A = a;
+                B = b;
+            }
+
+            public int GetB()
+            {
+                return B;
+            }
+
+            public int A { get; private set; }
+            public int B { private get; set; }
+        }
+
+        private SerializerImpl serializer;
+    }
+}
