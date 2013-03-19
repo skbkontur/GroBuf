72a6a2ba1f711f5a910767aab014048232087d0c
ichevdar <ichevdar@skbkontur.ru>
2013-03-19 11:20:39 +0000

    properties with getters or setters with parameters should not be handled

--- GroBuf/GroBuf/DataMembersExtracters/PropertiesExtractor.cs
+++ GroBuf/GroBuf/DataMembersExtracters/PropertiesExtractor.cs
@@ -8,7 +8,7 @@ namespace GroBuf.DataMembersExtracters
     {
         public MemberInfo[] GetMembers(Type type)
         {
-            return type.GetProperties(BindingFlags.Public | BindingFlags.Instance).Where(property => property.CanRead && property.CanWrite).ToArray();
+            return type.GetProperties(BindingFlags.Public | BindingFlags.Instance).Where(property => property.CanRead && property.GetGetMethod(true).GetParameters().Length == 0 && property.CanWrite && property.GetSetMethod(true).GetParameters().Length == 1).ToArray();
         }
     }
 }
