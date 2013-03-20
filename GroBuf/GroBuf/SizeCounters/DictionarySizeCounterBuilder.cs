ee948a6d0aa862b77949071f6f3e044116fb0ef3
ichevdar <ichevdar@skbkontur.ru>
2013-03-20 09:57:47 +0000

    Dictionary size computing/serializing optimization

--- GroBuf/GroBuf/SizeCounters/DictionarySizeCounterBuilder.cs
+++ GroBuf/GroBuf/SizeCounters/DictionarySizeCounterBuilder.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Reflection;
+using System.Reflection.Emit;
 
 using GrEmit;
 
@@ -42,37 +43,82 @@ namespace GroBuf.SizeCounters
             var il = context.Il;
             il.Ldc_I4(9); // stack: [9 = size] 9 = type code + data length + dictionary count
             context.LoadObj(); // stack: [size, obj]
-            var keyValueType = typeof(KeyValuePair<,>).MakeGenericType(keyType, valueType);
-            var enumeratorType = typeof(IEnumerator<>).MakeGenericType(keyValueType);
-            var enumerator = il.DeclareLocal(enumeratorType);
-            il.Call(typeof(IEnumerable<>).MakeGenericType(keyValueType).GetMethod("GetEnumerator"), Type); // stack: [size, obj.GetEnumerator()]
-            il.Dup(); // stack: [size, enumerator]
-            il.Stloc(enumerator); // enumerator = obj.GetEnumerator(); stack: [size, enumerator]
-            il.Call(typeof(IEnumerator).GetMethod("Reset"), typeof(IEnumerator)); // enumerator.Reset(); stack: [size]
-            var endLabel = il.DefineLabel("end");
-            il.Ldloc(enumerator);
-            il.Call(typeof(IEnumerator).GetMethod("MoveNext"), typeof(IEnumerator));
-            il.Brfalse(endLabel);
+            var count = il.DeclareLocal(typeof(int));
+            il.Ldfld(Type.GetField("count", BindingFlags.Instance | BindingFlags.NonPublic));
+            il.Stloc(count);
+            context.LoadObj(); // stack: [size, obj]
+            var entryType = Type.GetNestedType("Entry", BindingFlags.NonPublic).MakeGenericType(Type.GetGenericArguments());
+            var entries = il.DeclareLocal(entryType.MakeArrayType());
+            il.Ldfld(Type.GetField("entries", BindingFlags.Instance | BindingFlags.NonPublic));
+            il.Stloc(entries);
+
+            var i = il.DeclareLocal(typeof(int));
+            il.Ldc_I4(0); // stack: [9, 0]
+            il.Stloc(i); // i = 0; stack: [9]
             var cycleStartLabel = il.DefineLabel("cycleStart");
             il.MarkLabel(cycleStartLabel);
-            il.Ldloc(enumerator); // stack: [size, enumerator]
-            il.Call(enumeratorType.GetProperty("Current").GetGetMethod(), enumeratorType); // stack: [size, enumerator.Current]
-            var current = il.DeclareLocal(keyValueType);
-            il.Stloc(current);
-            il.Ldloca(current);
-            il.Call(keyValueType.GetProperty("Key").GetGetMethod(), keyValueType);
-            il.Ldc_I4(1);
-            context.CallSizeCounter(keyType);
-            il.Add();
-            il.Ldloca(current);
-            il.Call(keyValueType.GetProperty("Value").GetGetMethod(), keyValueType);
-            il.Ldc_I4(1);
-            context.CallSizeCounter(valueType);
-            il.Add();
-            il.Ldloc(enumerator);
-            il.Call(typeof(IEnumerator).GetMethod("MoveNext"), typeof(IEnumerator));
-            il.Brtrue(cycleStartLabel);
-            il.MarkLabel(endLabel);
+            il.Ldloc(entries); // stack: [size, entries]
+            il.Ldloc(i); // stack: [size, entries, i]
+            il.Ldelema(entryType); // stack: [size, &entries[i]]
+            il.Dup(); // stack: [size, &entries[i], &entries[i]]
+            var entry = il.DeclareLocal(entryType.MakeByRefType());
+            il.Stloc(entry); // entry = &entries[i]; stack: [size, entry]
+            il.Ldfld(entryType.GetField("hashCode")); // stack: [size, entry.hashCode]
+            il.Ldc_I4(0); // stack: [size, entry.hashCode, 0]
+            var nextLabel = il.DefineLabel("next");
+            il.Blt(typeof(int), nextLabel); // if(entry.hashCode < 0) goto next; stack: [size]
+            il.Ldloc(entry); // stack: [size, entry]
+            il.Ldfld(entryType.GetField("key")); // stack: [size, entry.key]
+            il.Ldc_I4(1); // stack: [size, obj[i], true]
+            context.CallSizeCounter(keyType); // stack: [size, writer(entry.key, true) = keySize]
+            il.Add(); // stack: [size + keySize]
+            
+            il.Ldloc(entry); // stack: [size, entry]
+            il.Ldfld(entryType.GetField("value")); // stack: [size, entry.value]
+            il.Ldc_I4(1); // stack: [size, obj[i], true]
+            context.CallSizeCounter(valueType); // stack: [size, writer(entry.value, true) = keySize]
+            il.Add(); // stack: [size + valueSize]
+
+            il.MarkLabel(nextLabel);
+            il.Ldloc(count); // stack: [size, count]
+            il.Ldloc(i); // stack: [size, count, i]
+            il.Ldc_I4(1); // stack: [size, count, i, 1]
+            il.Add(); // stack: [size, count, i + 1]
+            il.Dup(); // stack: [size, count, i + 1, i + 1]
+            il.Stloc(i); // i = i + 1; stack: [size, count, i]
+            il.Bgt(typeof(int), cycleStartLabel); // if(count > i) goto cycleStart; stack: [size]
+
+//            var keyValueType = typeof(KeyValuePair<,>).MakeGenericType(keyType, valueType);
+//            var enumeratorType = typeof(IEnumerator<>).MakeGenericType(keyValueType);
+//            var enumerator = il.DeclareLocal(enumeratorType);
+//            il.Call(typeof(IEnumerable<>).MakeGenericType(keyValueType).GetMethod("GetEnumerator"), Type); // stack: [size, obj.GetEnumerator()]
+//            il.Dup(); // stack: [size, enumerator]
+//            il.Stloc(enumerator); // enumerator = obj.GetEnumerator(); stack: [size, enumerator]
+//            il.Call(typeof(IEnumerator).GetMethod("Reset"), typeof(IEnumerator)); // enumerator.Reset(); stack: [size]
+//            var endLabel = il.DefineLabel("end");
+//            il.Ldloc(enumerator);
+//            il.Call(typeof(IEnumerator).GetMethod("MoveNext"), typeof(IEnumerator));
+//            il.Brfalse(endLabel);
+//            var cycleStartLabel = il.DefineLabel("cycleStart");
+//            il.MarkLabel(cycleStartLabel);
+//            il.Ldloc(enumerator); // stack: [size, enumerator]
+//            il.Call(enumeratorType.GetProperty("Current").GetGetMethod(), enumeratorType); // stack: [size, enumerator.Current]
+//            var current = il.DeclareLocal(keyValueType);
+//            il.Stloc(current);
+//            il.Ldloca(current);
+//            il.Call(keyValueType.GetProperty("Key").GetGetMethod(), keyValueType);
+//            il.Ldc_I4(1);
+//            context.CallSizeCounter(keyType);
+//            il.Add();
+//            il.Ldloca(current);
+//            il.Call(keyValueType.GetProperty("Value").GetGetMethod(), keyValueType);
+//            il.Ldc_I4(1);
+//            context.CallSizeCounter(valueType);
+//            il.Add();
+//            il.Ldloc(enumerator);
+//            il.Call(typeof(IEnumerator).GetMethod("MoveNext"), typeof(IEnumerator));
+//            il.Brtrue(cycleStartLabel);
+//            il.MarkLabel(endLabel);
         }
 
         private readonly Type keyType;
--- GroBuf/Tests/TestDictionary.cs
+++ GroBuf/Tests/TestDictionary.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 
 using GroBuf.DataMembersExtracters;
 
@@ -43,6 +44,33 @@ namespace GroBuf.Tests
             Assert.AreEqual(2, dict2.Count);
             Assert.AreEqual(1, dict2["1"]);
             Assert.AreEqual(2, dict2["2"]);
+        }
+
+        [Test]
+        public void TestPerformance()
+        {
+            var dict = new Dictionary<int, int>();
+            for(int i = 0; i < 10000; ++i)
+                dict.Add(i, i);
+            Console.WriteLine(serializer.GetSize(dict));
+            serializer.Deserialize<Dictionary<int, int>>(serializer.Serialize(dict));
+            var stopwatch = Stopwatch.StartNew();
+            const int iterations = 10000;
+            for(int iter = 0; iter < iterations; ++iter)
+                serializer.GetSize(dict);
+            var elapsed = stopwatch.Elapsed;
+            Console.WriteLine("Size computing: " + elapsed.TotalMilliseconds * 1000 / iterations + " microseconds (" + Math.Round(1000.0 * iterations / elapsed.TotalMilliseconds) + " size computations per second)");
+            stopwatch = Stopwatch.StartNew();
+            for(int iter = 0; iter < iterations; ++iter)
+                serializer.Serialize(dict);
+            elapsed = stopwatch.Elapsed;
+            Console.WriteLine("Serializing: " + elapsed.TotalMilliseconds * 1000 / iterations + " microseconds (" + Math.Round(1000.0 * iterations / elapsed.TotalMilliseconds) + " serializations per second)");
+            var buf = serializer.Serialize(dict);
+            stopwatch = Stopwatch.StartNew();
+            for (int iter = 0; iter < iterations; ++iter)
+                serializer.Deserialize<Dictionary<int, int>>(buf);
+            elapsed = stopwatch.Elapsed;
+            Console.WriteLine("Deserializing: " + elapsed.TotalMilliseconds * 1000 / iterations + " microseconds (" + Math.Round(1000.0 * iterations / elapsed.TotalMilliseconds) + " deserializations per second)");
         }
     }
 }
