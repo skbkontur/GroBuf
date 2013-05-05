07d54cb819188e3c878c26735cb8dacd471dff09
ffominyh <ffominyh@skbkontur.ru>
2013-05-05 16:03:14 +0000

    Serialize для 5 параметров

--- GroBuf/GroBuf/SerializerExtensions.cs
+++ GroBuf/GroBuf/SerializerExtensions.cs
@@ -34,5 +34,18 @@
             serializer.Serialize(param4, result, ref index);
             return result;
         }
+
+        public static byte[] Serialize<T1, T2, T3, T4, T5>(this ISerializer serializer, T1 param1, T2 param2, T3 param3, T4 param4, T5 param5)
+        {
+            int size = serializer.GetSize(param1) + serializer.GetSize(param2) + serializer.GetSize(param3) + serializer.GetSize(param4) + serializer.GetSize(param5);
+            var result = new byte[size];
+            int index = 0;
+            serializer.Serialize(param1, result, ref index);
+            serializer.Serialize(param2, result, ref index);
+            serializer.Serialize(param3, result, ref index);
+            serializer.Serialize(param4, result, ref index);
+            serializer.Serialize(param5, result, ref index);
+            return result;
+        }
     }
 }
