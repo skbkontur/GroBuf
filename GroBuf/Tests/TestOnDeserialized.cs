50b324ab641ec03189ca1ffd53d82ae62b365a06
Igor Chevdar <homuroll@yandex.ru>
2016-02-17 14:30:42 +0500

    added parameter to OnDeserialized

--- GroBuf/Tests/TestOnDeserialized.cs
+++ GroBuf/Tests/TestOnDeserialized.cs
@@ -1,4 +1,6 @@
-﻿using System.Runtime.Serialization;
+﻿using System;
+using System.Runtime.Remoting;
+using System.Runtime.Serialization;
 
 using GroBuf.DataMembersExtracters;
 
@@ -18,23 +20,44 @@ namespace GroBuf.Tests
         }
 
         [Test]
-        public void Test()
+        public void TestOk()
         {
             var o = new TestData {S = "zzz", DeserializedCalled = false};
             var data = serializer.Serialize(o);
             var oo = serializer.Deserialize<TestData>(data);
             Assert.IsTrue(oo.DeserializedCalled);
+            Assert.That(oo.s, Is.EqualTo("zzz"));
+        }
+
+        [Test]
+        public void TestBadMethod()
+        {
+            var o = new BadData {S = "zzz"};
+            var data = serializer.Serialize(o);
+            Assert.Throws<InvalidOperationException>(() => serializer.Deserialize<BadData>(data));
         }
 
         private class TestData
         {
             public string S { get; set; }
             public bool DeserializedCalled { get; set; }
+            public string s;
+
+            [OnDeserialized]
+            private void OnDeserialized(StreamingContext context)
+            {
+                DeserializedCalled = true;
+                s = S;
+            }
+        }
+
+        private class BadData
+        {
+            public string S { get; set; }
 
             [OnDeserialized]
             private void OnDeserialized()
             {
-                DeserializedCalled = true;
             }
         }
     }
