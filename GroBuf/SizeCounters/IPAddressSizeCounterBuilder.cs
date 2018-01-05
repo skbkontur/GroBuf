6f065569e8fc9869a0c7d89aeddbbdff2ad750bf
avk <Andrew.Kostousov@gmail.com>
2018-01-05 19:20:41 +0500

    use public IPAddress.AddressFamily property as it also works for netcoreapp2.0

--- GroBuf/SizeCounters/IPAddressSizeCounterBuilder.cs
+++ GroBuf/SizeCounters/IPAddressSizeCounterBuilder.cs
@@ -21,14 +21,14 @@ namespace GroBuf.SizeCounters
 
             il.Ldc_I4(4); // stack: [4]
             context.LoadObj(); // stack: [4, obj]
-            il.Ldfld(Type.GetField("m_Family", BindingFlags.Instance | BindingFlags.NonPublic)); // stack: [4, obj.m_Family]
-            il.Ldc_I4((int)AddressFamily.InterNetworkV6); // stack: [4, obj.m_Family, AddressFamily.InterNetworkV6]
-            il.Ceq(); // stack: [4, obj.m_Family == AddressFamily.InterNetworkV6]
-            il.Ldc_I4(1); // stack: [4, obj.m_Family == AddressFamily.InterNetworkV6, 1]
-            il.Shl(); // stack: [4, (obj.m_Family == AddressFamily.InterNetworkV6) << 1]
-            il.Shl(); // stack: [4 << ((obj.m_Family == AddressFamily.InterNetworkV6) << 1)]
-            il.Ldc_I4(5); // stack: [4 << ((obj.m_Family == AddressFamily.InterNetworkV6) << 1), 5]
-            il.Add(); // stack: [4 << ((obj.m_Family == AddressFamily.InterNetworkV6) << 1) + 5]
+            il.Call(Type.GetProperty("AddressFamily", BindingFlags.Instance | BindingFlags.Public).GetGetMethod()); // stack: [4, obj.AddressFamily]
+            il.Ldc_I4((int)AddressFamily.InterNetworkV6); // stack: [4, obj.AddressFamily, AddressFamily.InterNetworkV6]
+            il.Ceq(); // stack: [4, obj.AddressFamily == AddressFamily.InterNetworkV6]
+            il.Ldc_I4(1); // stack: [4, obj.AddressFamily == AddressFamily.InterNetworkV6, 1]
+            il.Shl(); // stack: [4, (obj.AddressFamily == AddressFamily.InterNetworkV6) << 1]
+            il.Shl(); // stack: [4 << ((obj.AddressFamily == AddressFamily.InterNetworkV6) << 1)]
+            il.Ldc_I4(5); // stack: [4 << ((obj.AddressFamily == AddressFamily.InterNetworkV6) << 1), 5]
+            il.Add(); // stack: [4 << ((obj.AddressFamily == AddressFamily.InterNetworkV6) << 1) + 5]
         }
 
         protected override bool IsReference { get { return false; } }
